612bfd8eecfbf76661d3e36da1de199ba71ce7ca
oreissig <oreissig+github@gmail.com>
2013-05-25 15:17:25 +0200

    new Optimization removes nodes without effect

---
+++ BFRC-Opts/src/bfrc/opts/RemoveNops.java
@@ -0,0 +1,32 @@
+package bfrc.opts;
+
+import java.util.Deque;
+
+import bfrc.ast.AbstractTreeWalker;
+import bfrc.ast.BlockNode;
+import bfrc.ast.ChangeNode;
+import bfrc.ast.Node;
+import bfrc.optimizer.Optimizer;
+import bfrc.optimizer.OptimizerException;
+
+/**
+ * This optimization removes relative {@link ChangeNode ChangeNodes}
+ * (POINTER, VALUE), whose {@link ChangeNode#change change} is zero.
+ * <p>
+ * This should be applied after {@link FoldMultiOps} has been performed.
+ * 
+ * @author oreissig
+ */
+public class RemoveNops extends AbstractTreeWalker<OptimizerException>
+		implements Optimizer {
+
+	@Override
+	protected boolean visit(Node n, Deque<BlockNode> stack) {
+		if (n instanceof ChangeNode) {
+			ChangeNode cn = (ChangeNode) n;
+			if (!cn.absolute && cn.change == 0)
+				return false;
+		}
+		return true;
+	}
+}
